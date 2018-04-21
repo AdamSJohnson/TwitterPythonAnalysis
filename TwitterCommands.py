f25a71372bb8538438460e4b9b536c353faf75c7
EZ <zaireric@gmail.com>
2018-04-21 19:07:26 -0400

    another one

--- TwitterCommands.py
+++ TwitterCommands.py
@@ -6,20 +6,7 @@ import random
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, Stream
 from TwitterAPIKeys import API
-from TwitterAPIKeys import CONSUMER_KEY as CK
-from TwitterAPIKeys import SECRET_KEY as SK
-from TwitterAPIKeys import ACCESS_TOKEN as AT
-from TwitterAPIKeys import SECRET_TOKEN as ST
 
-
-#fetch request token for twitter.
-#return type: void
-def requestToken(auth_handler):
-    try:
-        redirect_url = auth_handler.get_authorization_url()
-    except:
-        print ("Error! Failed to get request token.")
-        exit()
 
 '''
 returns a list of a bunch of random tweets#
@@ -64,23 +51,3 @@ print start
 print end
 list_ = getUserTweets("realdonaldtrump", start, end, 15)
 print len(list_)
-    while i < number_of_tweets:
-        random_tweet = random.choice(tweets)
-        if (random_tweet in list_of_tweets) or (date(random_tweet.created_at) < start) or (date(random_tweet.created_at) > end):
-            i -= 1
-        else:
-            list_of_tweets.append(random_tweet.text)
-            i += 1
-    return list_of_tweets
-
-#connect to the tweepy api
-auth = tweepy.OAuthHandler(CK, SK)
-auth.set_access_token(AT, ST)
-#auth = tweepy.API(auth)
-requestToken(auth)
-api = tweepy.API(auth)
-
-
-start = datetime.date(2016, 12, 31)
-end = datetime.date(2018, 12, 31)
-#getUserTweets("realdonaldtrump", start, end, 100)
