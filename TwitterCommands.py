cf74a9d9a57941483350f2e17b3d8ab6fac00e54
EZ <zaireric@gmail.com>
2018-04-21 21:11:34 -0400

    another one

--- TwitterCommands.py
+++ TwitterCommands.py
@@ -16,39 +16,21 @@ paramaters:
         string username(twitter username)
         string start(beginning time range to grab tweet from)
         string end(ending time range to grab tweet from)
-        int sample_size( amount of tweets you want to grab)
 return type: list[] (of tweets as text)    
 '''
-def getUserTweets(username, start, end, sample_size):
+def getUserTweets(username, start, end):
     user = API.get_user(username)
     tweets = API.user_timeline(screen_name = username, count = user.statuses_count, include_rts = True, tweet_mode="extended")
     list_of_tweets = []
-    
-    #grab all of the random tweets made by the user.
-    i = 0
-    while i < len(tweets) and len(list_of_tweets) <= sample_size:
-        random_tweet = random.choice(tweets)
-        if (random_tweet not in list_of_tweets) and (start <= random_tweet.created_at) and (end >= random_tweet.created_at):
-            list_of_tweets.append(random_tweet)
-        i += 1
-    #go throuvgh the list of tweet objects and parse out all of the tweet.text
-    #take all of that and add it to a list
-    #return the list of tweet text right after
-    list_of_tweet_text = [[tweet.full_text] for tweet in list_of_tweets]
-    return list_of_tweet_text
+    for tweet in tweets:
+        if (start <= tweet.created_at) and (end >= tweet.created_at):
+            list_of_tweets.append(tweet.full_text)
+    return list_of_tweets
 
-'''
-go through all the searches for a givn hashtag.
-paramaters:
-        string hashtag(twitter hashtag)
-        string start(beginning time range to grab tweet from)
-        string end(ending time range to grab tweet from)
-        int sample_size( amount of tweets you want to grab)
-return type: list[] (of tweets as text)    
 
 '''
 def getHashtagTweets(hashtag, start, end, sample_size):
-    search_results = API.search(hashtag)
+    search_results = API.search(hashtag, text_mode="full_text")
     list_of_tweets = []
 
     i = 0
@@ -62,13 +44,15 @@ def getHashtagTweets(hashtag, start, end, sample_size):
     for i in list_of_tweets:
         list_of_tweet_text.append(i.text)
     return list_of_tweet_text
+'''
 
 
-
+#____________________________________________________________
 start = datetime.datetime(2016, 1, 1, 0, 0)
 end = datetime.datetime(2019, 12, 31, 23, 59)
-list_ = getUserTweets("realdonaldtrump", start, end, 15)
-
-list2 = getHashtagTweets("#cool", start, end, 15)
-for i in list2:
+list_ = getUserTweets("realdonaldtrump", start, end)
+for i in list_:
     print i, "\n\n"
+#list2 = getHashtagTweets("#cool", start, end, 15)
+#for i in list2:
+ #   print i, "\n\n"
